a8e94c8300dbea508927fd3eeeae19a848563080
db0w <diego.bow@proton.me>
2023-06-29 12:28:15 +0200

    put deleted function's body into while loop. No need function.

--- calculo.py
+++ calculo.py
@@ -1,14 +1,10 @@
 grfile = open('gr_semanal.txt', "a")
 
-#Asign number a var and return this var like an int
-def enter_action():
-  print('Qué quieres hacer?\n1 = Agregar alimento\n2 = Agregar cantidad\n3 = Salir')
-  action = input('Seleccione: ')
-  return int(action)
 
 #Loop for make easy put all data, choose number 3 to break the loop
 while True:
-  action = enter_action()
+  print('Qué quieres hacer?\n1 = Agregar alimento\n2 = Agregar cantidad\n3 = Salir')
+  action = input('Seleccione: ')
   if action == 1:
     food = input('Introduzca un alimento: ')
   elif action == 3:

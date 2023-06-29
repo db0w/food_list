0445350aa729f6bf7a4c95c4210ae5075d58f776
db0w <diego.bow@proton.me>
2023-06-29 12:36:50 +0200

    put correct indentation, 4 spaces

--- calculo.py
+++ calculo.py
@@ -3,9 +3,9 @@ grfile = open('gr_semanal.txt', "a")
 
 #Loop for make easy put all data, choose number 3 to break the loop
 while True:
-  print('Qué quieres hacer?\n1 = Agregar alimento\n2 = Agregar cantidad\n3 = Salir')
-  action = input('Seleccione: ')
-  if action == 1:
-    food = input('Introduzca un alimento: ')
-  elif action == 3:
-    break
+    print('Qué quieres hacer?\n1 = Agregar alimento\n2 = Agregar cantidad\n3 = Salir')
+    action = input('Seleccione: ')
+    if action == 1:
+        food = input('Introduzca un alimento: ')
+    elif action == 3:
+        break

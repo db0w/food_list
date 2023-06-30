b4e290a47618a0b3b50aa3a4ee538e8b517f5524
db0w <diego.bow@proton.me>
2023-06-30 09:32:18 +0200

    Correct type action input to int

--- calculo.py
+++ calculo.py
@@ -4,7 +4,7 @@ grfile = open('gr_semanal.txt', "a")
 #Loop for make easy put all data, choose number 3 to break the loop
 while True:
     print('Qué quieres hacer?\n1 = Agregar alimento\n2 = Agregar cantidad\n3 = Salir')
-    action = input('Seleccione: ')
+    action = int(input('Seleccione: '))
     if action == 1:
         food = input('Introduzca un alimento: ')
     elif action == 3:

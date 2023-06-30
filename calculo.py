0dad622d653a2a755d12fcdcc68cdc680e3e7761
db0w <diego.bow@proton.me>
2023-06-30 12:27:12 +0200

    If statement to avoid write empty dictionary and enter an entered food

--- calculo.py
+++ calculo.py
@@ -9,7 +9,11 @@ while True:
     print('Qué quieres hacer?\n1 = Agregar alimento\n2 = Agregar cantidad\n3 = Guardar en diccionario\n4 = Salir')
     action = int(input('Seleccione: '))
     if action == 1:
-        food = input('Introduzca un alimento: ')
+        food = input('Introduzca un alimento en minúsculas, sin acentos, singular y con "_" como separación: ')
+        for key in lstfood.keys():
+            if food == key:
+                print('Alimento ya agregado')
+                food = None
     if action == 2:
         #Loop for enter and sum infinite quantities, press 0 to break it
         while True:
@@ -28,8 +32,8 @@ while True:
             print('Guardado:', lstfood)
     elif action == 4:
         break
-
-
-
-
-grfile.write(str(lstfood))
+    
+if lstfood != {}:
+    grfile.write(str(lstfood))
+else:
+    print(str.upper('Diccionario vacío!'))

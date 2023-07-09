ba9050db387871ed616d67a400811fca0f11a8a9
db0w <diego.bow@proton.me>
2023-07-09 08:00:59 +0200

    Turn input to str and turn exit option writing 'exit' word not pressing number 4

--- calculo.py
+++ calculo.py
@@ -7,9 +7,9 @@ food = None
 lstfood = dict()
 
 while True:
-    print('Qué quieres hacer?\n1 = Agregar alimento\n2 = Agregar cantidad\n3 = Guardar en diccionario\n4 = Salir\n5 = Mostrar lista')
-    action = int(input('Seleccione: '))
-    if action == 1:
+    print('Qué quieres hacer?\n1 = Agregar alimento\n2 = Agregar cantidad\n3 = Guardar en diccionario\n4 = Mostrar lista\nexit = Salir')
+    action = input('Seleccione: ')
+    if action == '1':
         food = input('Introduzca un alimento en minúsculas, sin acentos, singular y con "_" como separación: ')
         #iterate throw dictionay to find food already entered
         for key,value in lstfood.items():
@@ -17,7 +17,7 @@ while True:
                 print('Alimento ya agregado, actualizar', key, value)
                 food = key
                 gramage = int(value)
-    if action == 2:
+    if action == '2':
         #Loop for enter and sum infinite quantities, press 0 to break it
         while True:
             print('Para salir introducir 0')
@@ -26,17 +26,17 @@ while True:
                 break
             else:
                 gramage += gr
-    elif action == 3:
+    elif action == '3':
         if gramage is None or food is None:
             print(str.upper("Atención ningún gramo o alimento añadido!"))
         else:
             lstfood[food] = str(gramage)
             gramage = 0
             print('Guardado:', lstfood)
-    elif action == 4:
-        break
-    elif action == 5:
+    elif action == '4':
         print(lstfood)
+    elif action == 'exit':
+        break
 
 if lstfood != {}:
     wfile.write(str(lstfood.items()))
